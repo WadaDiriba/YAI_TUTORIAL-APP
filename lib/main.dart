4e1e95a11d3a286c047cd8141955bb7ffff59cd9
WadaDiriba <wedadiriba@gmail.com>
2025-11-16 00:11:46 +0900

    Added thing

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,130 @@
-
-
 import 'package:flutter/material.dart';
 
 void main(){
 
-  runApp( const MyApp());
+  runApp( ProfileAPP());
+
+
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ProfileAPP extends StatelessWidget{
+
+  const ProfileAPP({super.key});
+
+
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+
+  Widget build(BuildContext context){
+
+    return MaterialApp(
+          home: Scaffold(
+           appBar:AppBar(
+
+            title: Text("Profile APP"),
+            
+            ),
+
+           body:Center(
+
+            child: Column(
+               
+               
+              children: [
+                 CircleAvatar(
+                  radius: 50,
+
+                  child: Icon(Icons.person,size:60),
+
+                     
+                 ),
+
+                 SizedBox(height: 16),
+
+                 Text("Wada Diriba",style: TextStyle(
+                  fontSize: 24,
+                 fontWeight: FontWeight.bold),),
+                 Row(
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+                 
+                    children: [
+                      Icon(Icons.email),
+                      SizedBox(width: 8),
+
+                      Text("wedadiriba@gmail.com",style: TextStyle(
+                       fontSize: 24,
+                        fontWeight: FontWeight.w100),
+                        
+                        ),
+                    ],
+
+                 ),
+
+                     Row(
+
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                      Icon(Icons.phone),
+                      SizedBox(width: 8),
+
+                      Text("0985159172",style: TextStyle(
+                       fontSize: 24,
+                        fontWeight: FontWeight.w100),
+                        
+                        ),
+                      ],
+                      ),
+
+                        SizedBox(height: 16),
+
+                        Row(
+                           mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                         Icon(Icons.location_on),
+
+                      SizedBox(width: 8),
+
+                       Text("Jimmaa, Ethiopia",style: TextStyle(
+                       fontSize: 24,
+                        fontWeight: FontWeight.w100),
+                        
+                        ),
+                        ]
+                         ),
+
+                         Row(
+
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                        Icon(Icons.work),
+                      SizedBox(width: 8),
+
+                        Text("Flutter Developer",style: TextStyle(
+                        fontSize: 24,
+                          fontWeight: FontWeight.w100),
+                          
+                          ),
+                        ]
+                         )
+
+
+
+
+
+
+
+                      
+                      ]
+                  
+
+                  )
+              
+            ),
+           )
+           
+          );
+
+    
   }
 }
--- lib/part_one.dart
+++ lib/part_one.dart
@@ -5,24 +5,171 @@
 //   print("I am dart programmer");
 // }
 
-//PART TWO  HOW TO CREATE VARIABLES IN DART
+//PART TWO  HOW TO CREATE VARIABLES IN DART  AND DATYPE
+
+// void main(){
+
+//   //string variable
+//   String name = "Wada";
+
+//   //integer variable
+//   int age = 25;
+
+//   //double variable
+//   double height = 5.9;
+
+//   //boolean variable
+//   bool isStudent = true;
+
+//   print("Name: $name");
+//   print("Age: $age");
+//   print("Height: $height m");
+//   print("Is Student: $isStudent");
+// }
+
+//PART THREE CONST AND FINAL
+
+// final city="Jimma";
+
+// const PI=3.14;
+
+
+// FOUR STRING INTER POLATION  arhtimetic opraiton
 
 void main(){
 
-  //string variable
-  String name = "Wada";
+  String name="Wada";
+  int age=50;
 
-  //integer variable
-  int age = 25;
+  print("My name is ${name} and I am ${age} years old");
 
-  //double variable
-  double height = 5.9;
+  // arthimetic operation
 
-  //boolean variable
-  bool isStudent = true;
+  int a=10;
+  int b=20;
 
-  print("Name: $name");
-  print("Age: $age");
-  print("Height: $height m");
-  print("Is Student: $isStudent");
+  print(a+b);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+
+// if example
+if(age>=18){
+
+  print("You can vote you preisdent");
+
 }
+
+else{
+
+  print("You cannot vote any");
+
+}
+
+// elfe if 
+
+int score=90;
+
+if(score==80){
+
+  print("A-");
+}
+
+
+
+else if(score>=70){
+
+  print("B");
+}
+else if(score>=50){
+
+  print("C");
+}
+
+//  swich statement
+
+String day="Monday";
+
+switch(day){
+
+  case "Monday":
+
+  print(" the friat day");
+
+  case "Frday":
+
+  print(" the nearst weekend");
+
+  break;
+  default:
+
+  print("unknow day you entered");
+}
+ // for loop 
+
+ for (int i=1;i<=5;i++){
+
+  print("The number is  $i");
+
+ }
+
+  // while loop
+  int j=1;
+  while(j<=5){
+
+    print("while loop number is $j");
+    j++;
+  }
+
+ // do while loop
+  int k=5;
+  do{
+    print("the number:${k}");
+    k++;
+  } while(k>8);
+
+// map
+
+  Map <String,int> student={
+  "math":90,
+  "Pyhtisics":100,
+  "Chemo":60,
+  "Oromo":100,
+
+
+};
+
+print(student.keys);
+
+print(student.values);
+
+print(student.containsKey("math"));
+
+
+// class
+
+
+}
+
+// class stud{
+
+//   int age = 50;
+
+//   String name="Wada";
+
+//   String dept="Sofware Engineering";
+
+//      stud(
+
+//       this.age,
+
+//       this.dept,
+//       this.name,
+//      )
+
+
+// }
+
+
+
+
