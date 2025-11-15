95664690f2540e15851ca6f87dd479a27ad61a1a
WadaDiriba <wedadiriba@gmail.com>
2025-11-15 20:03:30 +0900

    added thing

---
+++ lib/part_one.dart
@@ -0,0 +1,28 @@
+// ONE  how to create void function for starting our app
+
+// void main(){
+
+//   print("I am dart programmer");
+// }
+
+//PART TWO  HOW TO CREATE VARIABLES IN DART
+
+void main(){
+
+  //string variable
+  String name = "Wada";
+
+  //integer variable
+  int age = 25;
+
+  //double variable
+  double height = 5.9;
+
+  //boolean variable
+  bool isStudent = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height m");
+  print("Is Student: $isStudent");
+}
